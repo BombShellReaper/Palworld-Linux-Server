2def5d111c92574aea3682c8fcac3de140e547ef
BombShellReaper <165164911+BombShellReaper@users.noreply.github.com>
2024-09-25 20:07:30 -0400

    update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,5 @@
+ #!/bin/bash
 
+# This script will update the server, but you might have to make edits to the dir path
+
+steamcmd +force_install_dir /home/steam/.steam/steamapps/common/PalServer/ +login anonymous +app_update 2394010 validate +quit
