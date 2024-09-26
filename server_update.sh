3285a0c252dbb6a2ca65c1dede1c0ddb6a89e318
BombShellReaper <165164911+BombShellReaper@users.noreply.github.com>
2024-09-25 20:17:40 -0400

    Create server_update.sh

---
+++ server_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This is a script to update and reboot the server
+# Uncommnet anu of the following commands to allow them to run
+
+# Update package list
+# apt update 
+
+# Upgrade Packages
+# apt full-upgrade -y
+
+# Clean up
+# apt autoremove -y
+
+# Reboot the system
+reboot
