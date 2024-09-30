ea666fb7a0ccbe084d281e8dd19660d3d0e0214e
BombShellReaper <165164911+BombShellReaper@users.noreply.github.com>
2024-09-30 12:47:47 -0400

    Update palworld.sh

--- palworld.sh
+++ palworld.sh
@@ -2,6 +2,7 @@
 
 # Log file
 LOGFILE="/path/to/your/logfile.txt"  # Update with your log file path
+DIRPATH="/path/to/your/server" # Update with the directory containing PalServer.sh
 
 # Function to log messages with date/time
 log() {
@@ -11,7 +12,7 @@ log() {
 # Update PalWorld using steamcmd
 {
     log "Updating PalWorld..."
-    if /usr/games/steamcmd +force_install_dir "/path/to/your/game" +login anonymous +app_update 2394010 validate +quit; then  # Update with path to your game
+    if /usr/games/steamcmd +force_install_dir "$DIRPATH" +login anonymous +app_update 2394010 validate +quit; then
         log "Update completed."
     else
         log "Update failed."
@@ -20,11 +21,10 @@ log() {
 
     # Start the PalWorld server
     log "Starting PalWorld server..."
-    if /usr/bin/screen -dmS PalWorld "/path/to/your/game/PalServer.sh" -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS -PublicLobby; then  # Update with path to your game
+    if /usr/bin/screen -dmS PalWorld "$DIRPATH/PalServer.sh" -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS -PublicLobby; then  # Update with path to your script
         log "PalWorld server started successfully."
     else
         log "Failed to start PalWorld server."
         exit 1
     fi
 } 2>&1 | tee -a "$LOGFILE"
-
